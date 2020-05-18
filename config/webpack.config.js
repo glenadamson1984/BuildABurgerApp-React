611ad8130098e28b1ead7dca794b8ebf1be95eeb
Glen Adamson <glenadamson@glens-air.home>
2020-05-18 20:54:41 +0100

    fix webpack.config.js - did not need changes

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -81,11 +81,7 @@ module.exports = function(webpackEnv) {
       },
       {
         loader: require.resolve('css-loader'),
-        options: {
-          modules: true,
-          importLoaders: 1,
-          localIdentName: '[name]__[local]__[hash:base64:5]'
-        },
+        options: cssOptions,
       },
       {
         // Options for PostCSS as we reference these options twice
